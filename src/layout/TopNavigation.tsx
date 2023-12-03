f66fd83a00ffffee57207da4a150dbdaf1f9c51f
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-12-03 10:09:14 +0800

    added user avatar, fixed nav

--- src/layout/TopNavigation.tsx
+++ src/layout/TopNavigation.tsx
@@ -1,3 +1,4 @@
+import UserAvatar from "@/components/UserAvatar";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import Link from "next/link";
@@ -9,8 +10,8 @@ interface TopNavigationProps {
 
 const TopNavigation: React.FC<TopNavigationProps> = ({ open, setOpen }) => {
   return (
-    <nav className="">
-      <div className="fixed left-0 right-0 top-0 mb-4 flex h-16 items-center border-b border-b-white bg-black">
+    <nav className="fixed left-0 right-0 top-0 flex h-16 items-center justify-between border-b border-b-white bg-black p-2">
+      <div className="flex items-center  ">
         <Button
           onClick={() => setOpen((pre) => !pre)}
           className="rounded-full sm:hidden"
@@ -22,6 +23,9 @@ const TopNavigation: React.FC<TopNavigationProps> = ({ open, setOpen }) => {
         <Link href={"/app"} className="mx-2 text-2xl">
           in<strong>1</strong>space<strong>.</strong>
         </Link>
+      </div>
+      <div>
+        <UserAvatar />
       </div>
     </nav>
   );
